7d7e821de1d32ce6b0311f646a250e408a19121c
lllssskkk <lusikaistudy@gmail.com>
2023-05-06 17:57:51 +0200

    remove redundant files

--- src/helium/Helium/Parse.hs
+++ src/helium/Helium/Parse.hs
@@ -10,21 +10,17 @@ import Helium.Main.CompileUtils qualified as Helium
 import Helium.Utility.Helium qualified as H
 import Language.Haskell.Generated.Syntax qualified as Syn
 
--- import Language.Haskell.Utility.Utils qualified as Language
-
 type Type = String
+type Name = String
 
 -- ! Should we consider using Text instead of String, to bring better performance
-parse :: Syn.Name -> Type -> String -> Either String H.Module
+parse :: Name -> Type -> String -> Either String H.Module
 parse fnName fnType fnImplementation = H.compile (if isDefined then typeDecl ++ fnImplementation' else fnImplementation') H.defaultOptions
   where
-    fnImplementation' :: String 
+    fnImplementation' :: String
     fnImplementation' = escape fnImplementation
-    fun :: String 
-    fun = showName fnName
-    isDefined = isJust (H.typeOf fun fnImplementation')
-    typeDecl = fun ++ " :: " ++ fnType ++ "\n"
-
+    isDefined = isJust (H.typeOf fnName fnImplementation')
+    typeDecl = fnName ++ " :: " ++ fnType ++ "\n"
 
 -- | Help functions
 -- ! escape converts string and text, back and forth. Can we do everything in Text mode?
@@ -34,10 +30,3 @@ escape = inText (\txt -> foldr (\(a, b) txt' -> T.replace a b txt') txt rs)
     rs = [("\\n", "\n"), ("\\\"", "\"")]
     t = T.pack
     inText f = T.unpack . f . t
-
-showName :: Syn.Name -> String
-showName n =
-    case n of
-        Syn.Ident s -> s
-        Syn.Operator s -> s
-        Syn.Special s -> s

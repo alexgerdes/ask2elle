12bc6e0f3beda43913c8f912aedd755bc8c5abac
lllssskkk <lusikaistudy@gmail.com>
2023-05-06 17:58:22 +0200

    remove redundant files

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,5 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE TypeApplications #-}
-
 module Main (main) where
-
-
 
 main :: IO ()
 main = do
-    let !x = 1 + 1
     putStrLn "Hello, Haskell!"
-
-
--- src/transformations/Transformations.hs
+++
@@ -1,7 +0,0 @@
-module Transformations (module X) where
-
-import Transformations.Desugar as X
-import Transformations.Inline as X
-import Transformations.Lambda as X
-import Transformations.Preprocess as X
-import Transformations.Substitute as X

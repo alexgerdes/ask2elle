a33fb21bbed9fae791431b644eb8d11d1897b898
lllssskkk <lusikaistudy@gmail.com>
2023-11-15 20:08:14 +0100

    add nix flake environment support for customized GHC

--- flake.nix
+++ flake.nix
@@ -15,11 +15,7 @@
         let pkgs = nixpkgsFor system;
         in pkgs.stdenv.mkDerivation {
           name = "Standard-Dev-Environment-with-Utils";
-          buildInputs = [
-
-            pkgs.haskell.compiler.ghc928
-            pkgs.haskell.packages.ghc928.haskell-language-server
-          ] ++ (with pkgs; [
+          buildInputs = (with pkgs; [
             bashInteractive
             cabal-install
             fd
@@ -28,7 +24,7 @@
             nixfmt
             zlib
             sqlite
-            gcc
+            # gcc
           ]);
         };
 
@@ -40,21 +36,51 @@
             haskell-pkgs = pkgs.haskellPackages;
             project = (pkgs.stdenv.mkDerivation {
               name = "Haskell-Dev-Environment-with-Utils";
-              buildInputs = stdDevEnv.buildInputs
+              buildInputs = [
+                pkgs.haskell.compiler.ghc928
+                pkgs.haskell.packages.ghc928.haskell-language-server
+              ] ++ stdDevEnv.buildInputs
                 ++ (with haskell-pkgs; [ cabal-fmt fourmolu ]);
               shellHook = ''
-                export LD_LIBRARY_PATH=${pkgs.gcc.cc.lib}/lib:$LD_LIBRARY_PATH
                 export PATH=~/.cabal/bin:$PATH;
               '';
-
             });
 
           in project;
       };
-    in {
+                     #  export LD_LIBRARY_PATH=${pkgs.gcc.cc.lib}/lib:$LD_LIBRARY_PATH
+      debugGHC = rec {
+        projectFor = system:
+          let
+            pkgs = nixpkgsFor system;
+            stdDevEnv = mkDevEnv system;
+            haskell-pkgs = pkgs.haskellPackages;
+            project = (pkgs.stdenv.mkDerivation {
+              name = "Haskell-Dev-Environment-with-Utils";
+              buildInputs = stdDevEnv.buildInputs ++ (with pkgs; [
+                # needed for running cabal with customized ghc
+                glibc
+                glib 
+                gmp 
+                numactl 
+                ncurses 
+              ]) ++ (with haskell-pkgs; [ cabal-fmt fourmolu ]);
+              shellHook = ''
+                export PATH=~/.cabal/bin:$PATH;
+                export PATH=~/Desktop/code/alexgerdes/ghc/_build/stage1/bin:$PATH;
+              '';
+            });
 
+          in project;
+      };
+
+    in {
+      debugGHC = perSystem (system: (debugGHC.projectFor system));
       haskell = perSystem (system: (haskell.projectFor system));
-      devShells = perSystem (system: { default = self.haskell.${system}; });
+      devShells = perSystem (system: {
+        default = self.haskell.${system};
+        debugGHC = self.debugGHC.${system};
+      });
       packages = perSystem (system: { default = self.haskell.${system}; });
     };
 }
